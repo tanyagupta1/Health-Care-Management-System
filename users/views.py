c5eba2a259068447e39693269f6b910b994f4f3c
ayush19030 <ayush19030@iiitd.ac.in>
2022-11-25 20:05:17 +0530

    temp changes

--- users/views.py
+++ users/views.py
@@ -27,7 +27,8 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request,'users/register.html',{'form':form})
+    return render(request , "users/sign.html")
+    # return render(request,'users/register.html',{'form':form})
 
 
 def login(request):
